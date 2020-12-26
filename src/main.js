c4bbff16ffc5d507c0c602fabf650dc16d643f99
”huazai“ <huazaizl@aliyun.com>
2020-12-26 13:51:31 +0800

    ”净美仕“

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
+// 根组件
 import App from './App.vue'
 import router from './router'
+// 路由
 import store from './store'
 
-Vue.config.productionTip = false
 
+Vue.config.productionTip = false
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
+Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
+
+// 创建Vue实例 挂载到app.js
 new Vue({
   router,
   store,
